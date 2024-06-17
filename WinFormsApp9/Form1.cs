06f6458eca8248795952ca916febe8b5d80e0b12
Sht1rl1Z <antonplyasovv@gmail.com>
2024-06-17 19:14:32 +0700

    GG

--- WinFormsApp9/Form1.cs
+++ WinFormsApp9/Form1.cs
@@ -9,7 +9,7 @@ namespace WinFormsApp9
         }
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            touch += 1;
+            touch += 5;
             label1.Text = touch.ToString();
             switch (touch)
             {
