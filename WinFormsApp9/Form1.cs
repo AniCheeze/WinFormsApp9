86d8ad16c3868d59e42984cc43f034ee92232dae
AniCheeze <daniil.avd06@mail.ru>
2024-06-17 18:44:50 +0700

    test

--- WinFormsApp9/Form1.cs
+++ WinFormsApp9/Form1.cs
@@ -2,9 +2,15 @@ namespace WinFormsApp9
 {
     public partial class Form1 : Form
     {
+        int touch;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            touch += 1;
         }
     }
 }
